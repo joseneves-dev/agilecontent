fe08a0d0ad2538865c09ade1100bcf646894af3f
José Neves <josecarlos-91@hotmail.com>
2024-12-30 22:47:12 +0000

    add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
+
+use Carbon\Carbon;
+
+use App\Models\User;
+use App\Models\Country;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +18,28 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $currentTimestamp = Carbon::now();
 
-        User::factory()->create([
+        $countries = [
+            ['name' => 'Spain', 'created_at' => $currentTimestamp, 'updated_at' => $currentTimestamp],
+            ['name' => 'Portugal', 'created_at' => $currentTimestamp, 'updated_at' => $currentTimestamp],
+            ['name' => 'France', 'created_at' => $currentTimestamp, 'updated_at' => $currentTimestamp],
+            ['name' => 'Germany', 'created_at' => $currentTimestamp, 'updated_at' => $currentTimestamp],
+            ['name' => 'Italy', 'created_at' => $currentTimestamp, 'updated_at' => $currentTimestamp],
+            ['name' => 'United Kingdom', 'created_at' => $currentTimestamp, 'updated_at' => $currentTimestamp],
+            ['name' => 'Netherlands', 'created_at' => $currentTimestamp, 'updated_at' => $currentTimestamp],
+            ['name' => 'Belgium', 'created_at' => $currentTimestamp, 'updated_at' => $currentTimestamp],
+            ['name' => 'Sweden', 'created_at' => $currentTimestamp, 'updated_at' => $currentTimestamp],
+            ['name' => 'Norway', 'created_at' => $currentTimestamp, 'updated_at' => $currentTimestamp],
+        ];
+
+        Country::insert($countries);
+
+        User::create([
             'name' => 'Test User',
+            'countryId' => 1,
+            'isActive' => 1,
+            'password' => Hash::make('123456789'),
             'email' => 'test@example.com',
         ]);
     }
