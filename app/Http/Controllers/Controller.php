05008f31d5e5b7dd5d7407cdf6f71e2bfd4dad7c
José Neves <josecarlos-91@hotmail.com>
2024-12-30 22:58:14 +0000

    update funtions

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+use Exception;
+
 abstract class Controller
 {
-    //
+    public function update(Request $request, $id)
+    {
+    
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function destroy($id)
+    {
+
+    }
 }
